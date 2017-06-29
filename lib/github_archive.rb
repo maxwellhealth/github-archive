da012368187cd7811f634b012d7821607d39986c
Brad Clark <bdashrad@gmail.com>
2017-06-29 13:50:04 -0400

    break out github client/auth

--- lib/github_archive.rb
+++ lib/github_archive.rb
@@ -1,7 +1,8 @@
 require 'github_archive/archive_repos'
+require 'github_archive/auth'
 require 'github_archive/organization'
 require 'github_archive/version'
 
+# Main module for Github Archive tool
 module GithubArchive
-
 end
--- lib/github_archive/organization/repos.rb
+++ lib/github_archive/organization/repos.rb
@@ -4,20 +4,9 @@ module GithubArchive
     class Repos
       attr_accessor :backup_count
       # archive an organizations repos
-      def archive(org, path, token)
-        token ||= ENV['GITHUB_TOKEN']
-        require 'octokit'
-        require 'open-uri'
-
+      def archive(org, path, client)
         # use current time for path
         path.concat Time.now.utc.strftime '%Y%m%d.%H%M%S'
-
-        # create a new github client
-        client = Octokit::Client.new(access_token: token)
-        # client = GithubArchive::Auth.new(token)
-
-        # create a new datadog client
-        # dog = Dogapi.Client.new(ENV['DOGAPI'])
 
         # get a collection of organization repos
         repos = client.organization_repositories org, per_page: 100
