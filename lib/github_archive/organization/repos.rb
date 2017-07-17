3155eb140ae08842b3d3d2589514444c3082145d
Brad Clark <bdashrad@gmail.com>
2017-07-17 11:05:18 -0400

    fix option order and move client out of cli

--- lib/github_archive/organization/repos.rb
+++ lib/github_archive/organization/repos.rb
@@ -2,36 +2,39 @@ module GithubArchive
   class Organization
     # archive github repos for an organization
     class Repos
-      attr_accessor :client, :dry_run, :path, :verbose
+      attr_accessor :token, :dry_run, :path, :verbose
       attr_reader :backup_count, :total_size
 
-      def initialize(client, dry_run, path, verbose)
+      def initialize(token, path, dry_run, verbose)
         @backup_count = 0
         @total_size = 0
         @path = path
-        @client = client
+        @token = token
         @dry_run = dry_run
         @verbose = verbose
+        @client = false
+        puts "Output File: #{path}" if dry_run || verbose
       end
 
       # archive an organizations repos
       def archive(org)
         # get a collection of organization repos
+        @client = GithubArchive::Auth.new(token).client
         repos = all_repos(org)
 
         # exit if we found no repos
         return unless repos.count > 0
 
-        o = GithubArchive::ArchiveRepos.new(client, dry_run, path, verbose)
+        o = GithubArchive::ArchiveRepos.new(token, path, dry_run, verbose)
         o.archive(repos)
         @backup_count = o.backup_count
         @total_size = o.total_size
       end
 
       def all_repos(org)
-        repos = client.organization_repositories org, per_page: 100
+        repos = @client.organization_repositories org, per_page: 100
         if repos.count >= 100
-          repos.concat client.last_response.rels[:next].get.data
+          repos.concat @client.last_response.rels[:next].get.data
         end
         repos
       end
--- lib/github_archive/user/repos.rb
+++ lib/github_archive/user/repos.rb
@@ -5,33 +5,36 @@ module GithubArchive
       attr_accessor :client, :dry_run, :path, :verbose
       attr_reader :backup_count, :total_size
 
-      def initialize(client, dry_run, path, verbose)
+      def initialize(token, path, dry_run, verbose)
         @backup_count = 0
         @total_size = 0
         @path = path
-        @client = client
+        @token = token
         @dry_run = dry_run
         @verbose = verbose
+        @client = false
+        puts "Output File: #{path}" if dry_run || verbose
       end
 
       # archive an organizations repos
       def archive(user)
         # get a collection of organization repos
+        @client = GithubArchive::Auth.new(token).client
         repos = all_repos(user)
 
         # exit if we found no repos
         return unless repos.count > 0
 
-        o = GithubArchive::ArchiveRepos.new(client, dry_run, path, verbose)
+        o = GithubArchive::ArchiveRepos.new(token, path, dry_run, verbose)
         o.archive(repos)
         @backup_count = o.backup_count
         @total_size = o.total_size
       end
 
       def all_repos(user)
-        repos = client.repositories user, per_page: 100
+        repos = @client.repositories user, per_page: 100
         if repos.count >= 100
-          repos.concat client.last_response.rels[:next].get.data
+          repos.concat @client.last_response.rels[:next].get.data
         end
         repos
       end
